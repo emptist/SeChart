278f0c89bcb1cdf4244dc035222930c936ffdce0
jigme1968 <jigme1968@gmail.com>
2020-10-16 05:28:19 +0800

    add dataset to chart

--- SeChart/Bar.swift
+++ SeChart/Bar.swift
@@ -9,15 +9,39 @@
 import SwiftUI
 import Charts
 
-struct Bar : UIViewRepresentable {
+struct Bar: UIViewRepresentable {
+    var entries: Array<BarChartDataEntry>
+    
     func makeUIView(context: Context) -> BarChartView {
-        <#code#>
+        let chart = BarChartView()
+        chart.data = addData()
+        return chart
+    }
+    
+    func addData() -> BarChartData {
+        let data = BarChartData()
+        let dataset = BarChartDataSet(entries: entries)
+        dataset.colors = [NSUIColor.green]
+        dataset.label = "MyData"
+        data.addDataSet(dataset)
+        return data
     }
     
     func updateUIView(_ uiView: BarChartView, context: Context) {
-        <#code#>
+        
     }
 
     typealias UIViewType = BarChartView
-    
+        
+}
+
+struct Bar_Previews: PreviewProvider {
+    static var previews: some View {
+        Bar(entries: [
+            BarChartDataEntry(x: 11, y: 12),
+            BarChartDataEntry(x: 12, y: 15),
+            BarChartDataEntry(x: 13, y: 16),
+            BarChartDataEntry(x: 14, y: 12)
+        ])
+    }
 }
