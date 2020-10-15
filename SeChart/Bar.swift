0d2cd170b50f0738d8f04529216892b9104f4577
jigme1968 <jigme1968@gmail.com>
2020-10-16 05:04:05 +0800

    create Bar

---
+++ SeChart/Bar.swift
@@ -0,0 +1,23 @@
+//
+//  Bar.swift
+//  SeChart
+//
+//  Created by jk on 2020/10/16.
+//  Copyright © 2020 ssl. All rights reserved.
+//
+
+import SwiftUI
+import Charts
+
+struct Bar : UIViewRepresentable {
+    func makeUIView(context: Context) -> BarChartView {
+        <#code#>
+    }
+    
+    func updateUIView(_ uiView: BarChartView, context: Context) {
+        <#code#>
+    }
+
+    typealias UIViewType = BarChartView
+    
+}
